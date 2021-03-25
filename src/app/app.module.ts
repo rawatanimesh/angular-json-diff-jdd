ec0269d40e7eeb150e03c2b71e43dbb8af7bb459
Rawat <ts-animesh.rawat@rakuten.com>
2021-03-25 14:34:05 +0530

    removed old diff library

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,6 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { DifferentiateModule } from '@sedeh/differentiate';
 
 @NgModule({
   declarations: [
@@ -11,8 +10,7 @@ import { DifferentiateModule } from '@sedeh/differentiate';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    DifferentiateModule
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
