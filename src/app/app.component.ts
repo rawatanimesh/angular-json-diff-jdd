453a4b47cefe26b5457a301db46d39a487784733
Animesh Rawat <animeshrawat@Anuradhas-MacBook-Pro.local>
2022-04-10 17:15:10 +0530

    updated UI for better UX

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,140 +1,138 @@
 import { Component, OnInit } from '@angular/core';
+import { browser } from 'protractor';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent implements OnInit {
-
   sampleArray2 = [
     {
-      "Aidan Gillen": {
-        "array": ["Game of Thron\"es", "The Wire"],
-        "string": "some string",
-        "int": 2,
-        "aboolean": true,
-        "boolean": false,
-        "object": {
-          "foo": "bar",
-          "object1": {
-            "new prop1": "new prop value",
-            "new prop2": "new prop value"
+      'Aidan Gillen': {
+        array: ['Game of Thron"es', 'The Wire'],
+        string: 'some string',
+        int: '2',
+        aboolean: true,
+        boolean: false,
+        object: {
+          foo: 'bar',
+          object1: {
+            'new prop1': 'new prop value',
+            'new prop2': 'new prop value',
           },
-          "object2": {
-            "new prop1": "new prop value"
+          object2: {
+            'new prop1': 'new prop value',
           },
-          "object3": {
-            "new prop1": "new prop value"
+          object3: {
+            'new prop1': 'new prop value',
           },
-          "object4": {
-            "new prop1": "new prop value"
-          }
-        }
+          object4: {
+            'new prop1': 'new prop value',
+          },
+        },
       },
-      "Amy Ryan": {
-        "one": "In Treatment",
-        "two": "The Wire"
+      'Amy Ryan': {
+        one: 'In Treatment',
+        two: 'The Wire',
       },
-      "Annie Fitzgerald": ["Big Love", "True Blood"],
-      "Anwan Glover": ["Treme", "The Wire"],
-      "Alexander Skarsgard": ["Generation Kill", "True Blood"],
-      "Clarke Peters": null,
-      "Bumbulbee": "n/a",
-      "id": "UHN1TKY100401133",
-      "date": "2021-02-09",
-      "status": "200",
-      "type": "MME"
+      'Annie Fitzgerald': ['Big Love', 'True Blood'],
+      'Anwan Glover': ['Treme', 'The Wire'],
+      'Alexander Skarsgard': ['Generation Kill', 'True Blood'],
+      'Clarke Peters': null,
+      Bumbulbee: 'n/a',
+      id: 'UHN1TKY100401133',
+      date: '2021-02-09',
+      status: '200',
+      type: 'MME',
     },
     {
-      "Aidan Gillen": {
-        "array": ["Game of Thron\"es", "The Wire"],
-        "string": "some string",
-        "int": 2,
-        "aboolean": true,
-        "boolean": true,
-        "object": {
-          "foo": "bar",
-          "object1": {
-            "new prop1": "new prop value"
+      'Aidan Gillen': {
+        array: ['Game of Thron"es', 'The Wire'],
+        string: 'some string',
+        int: 2,
+        aboolean: true,
+        boolean: true,
+        object: {
+          foo: 'bar',
+          object1: {
+            'new prop1': 'new prop value',
           },
-          "object2": {
-            "new prop1": "new prop value"
+          object2: {
+            'new prop1': 'new prop value',
           },
-          "object3": {
-            "new prop1": "new prop value"
+          object3: {
+            'new prop1': 'new prop value',
           },
-          "object4": {
-            "new prop1": "new prop value"
-          }
-        }
+          object4: {
+            'new prop1': 'new prop value',
+          },
+        },
       },
-      "Amy Ryan": {
-        "one": "In Treatment",
-        "two": "The Wire"
+      'Amy Ryan': {
+        one: 'In Treatment',
+        two: 'The Wire',
       },
-      "Annie Fitzgerald": ["Big Love", "True Blood","True Blood2"],
-      "Anwan Glover": ["Treme", "The Wire"],
-      "Alexander Skarsgard": ["Generation Kill", "True Blood"],
-      "Clarke Peters": null,
-      "Clarke Peters2": null,
-      "id": "UHN1TKY100401132",
-      "date": "2021-02-10",
-      "status": "200",
-      "type": "MME"
-    }
-  ]
+      'Annie Fitzgerald': ['Big Love', 'True Blood', 'True Blood2'],
+      'Anwan Glover': ['Treme', 'The Wire'],
+      'Alexander Skarsgard': ['Generation Kill', 'True Blood'],
+      'Clarke Peters': null,
+      'Clarke Peters2': null,
+      id: 'UHN1TKY100401132',
+      date: '2021-02-10',
+      status: '200',
+      type: 'MME',
+    },
+  ];
 
-  sampleArray = [{
-    id: 1,
-    "date": "2021-02-10",
-    "status": "200",
-    "type": "MME",
-    occupation: "engineer",
-    firstname: "masoud",
-    middleName: "saif",
-    lastname: "salehi",
-    address: {
-      street: "2345 blagio dr",
-      city: "Los Angeles",
-      countries: ["US","BS","DS","CS"]
+  sampleArray = [
+    {
+      id: 1,
+      date: '2021-02-10',
+      status: '200',
+      type: 'MME',
+      occupation: 'engineer',
+      firstname: 'masoud',
+      middleName: 'saif',
+      lastname: 'salehi',
+      address: {
+        street: '2345 blagio dr',
+        city: 'Los Angeles',
+        countries: ['US', 'BS', 'DS', 'CS'],
+      },
+      data: [['Better things yet to come!']],
     },
-    data: [
-      [
-        "Better things yet to come!"
-      ]
-    ]
-  },
-  {
-    id: 2,
-    occupation: "composer",
-    firstname: "daniel",
-    lastname: "salehi",
-    address: {
-      street: "545 beverly dr",
-      city: "Los Angeles",
-      countries: ["US","BS","CS"]
+    {
+      id: 2,
+      occupation: 'composer',
+      firstname: 'daniel',
+      lastname: 'salehi',
+      address: {
+        street: '545 beverly dr',
+        city: 'Los Angeles',
+        countries: ['US', 'BS', 'CS'],
+      },
+      data: [['Good things are here!']],
     },
-    data: [
-      [
-        "Good things are here!"
-      ]
-    ]
-  }
-]
+  ];
   diffReport: any;
 
-constructor() {}
+  constructor() {}
 
-  ngOnInit(){}
+  ngOnInit() {
+    // the asset files of library take time to be loaded on browser,
+    // hence timeout is used for demo purpose here
+    setTimeout(() => {
+      this.compare();
+    }, 200);
+  }
 
-  compare(){
+  compare() {
     document.getElementById('compare').click();
   }
 
-  differenceReport(event){
+  differenceReport(event) {
     console.log(event);
     this.diffReport = event;
   }
-
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { JsonComparisonComponent } from './json-comparison/json-comparison.component';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    JsonComparisonComponent
-  ],
+  declarations: [AppComponent, JsonComparisonComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
+    MatCardModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
